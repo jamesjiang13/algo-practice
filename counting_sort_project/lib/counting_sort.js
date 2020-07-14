a486015972f62b330dc3f176893703ae60395a66
James <james.jiang13@gmail.com>
2020-07-14 10:36:54 -0700

    Counting sort

--- counting_sort_project/lib/counting_sort.js
+++ counting_sort_project/lib/counting_sort.js
@@ -1,8 +1,19 @@
 function countingSort(arr, max) {
+	let countArray = new Array(max+1).fill(0);
+	let resArray = new Array;
+	arr.forEach((el) => countArray[el]++)
 
+	for (i=0; i <= countArray.length; i++) {
+		while (countArray[i] > 0) {
+			resArray.push(i);
+			countArray[i]--;
+		}
+	}
+
+	return resArray;
 }
 
-
 module.exports = {
-    countingSort
+	countingSort
 };
+
