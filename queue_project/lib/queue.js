e24588b14df91dff77ccedda25cf415b7ca98b8a
James <james.jiang13@gmail.com>
2020-07-15 21:22:04 -0700

    Completed queues

--- queue_project/lib/queue.js
+++ queue_project/lib/queue.js
@@ -21,11 +21,52 @@
 // -----------
 
 class Node {
-
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
 }
 
 class Queue {
+  constructor() {
+    this.front = null;
+    this.back = null;
+    this.length = 0;
+  }
 
+  enqueue(val) {
+    let newNode = new Node(val);
+    if (!this.front) {
+      this.front = newNode;
+      this.back = newNode;
+    } else {
+      this.back.next = newNode;
+      this.back = newNode;
+    }
+
+    this.length++;
+    return this.length;
+  }
+
+  dequeue() {
+    if (!this.front) return null;
+
+    let oldFront = this.front;
+    let newFront = oldFront.next;
+    this.front = newFront;
+    this.length--;
+
+    if (!this.length) {
+      this.front = null;
+      this.back = null;
+    }
+
+    return oldFront.value;
+  }
+
+  size() {
+    return this.length;
+  }
 }
 
 exports.Node = Node;
