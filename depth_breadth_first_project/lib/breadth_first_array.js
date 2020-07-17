65c9478c7306f67e53c8a6c36cf5fa7a5ed088e2
James <james.jiang13@gmail.com>
2020-07-16 19:19:30 -0700

    Completed DFS/BFS

--- depth_breadth_first_project/lib/breadth_first_array.js
+++ depth_breadth_first_project/lib/breadth_first_array.js
@@ -1,7 +1,17 @@
 function breadthFirstArray(root) {
+	if (!root) return [];
+	let res = [];
+	let queue = [root];
+	while (queue.length) {
+		let node = queue.shift();
+		res.push(node.val)
+		if (node.left) queue.push(node.left);
+		if (node.right) queue.push(node.right);
+	} 
+	return res;
+} 
 
-}
 
 module.exports = {
-    breadthFirstArray
+	breadthFirstArray
 };
--- depth_breadth_first_project/lib/depth_first_search.js
+++ depth_breadth_first_project/lib/depth_first_search.js
@@ -1,8 +1,16 @@
 function depthFirstSearch(root, targetVal) {
-
+	if (!root) return null;
+	let queue = [ root ];
+	while (queue.length) {
+		let node = queue.pop();
+		if (node.val === targetVal) return node;
+		if (node.right) queue.push(node.right)
+		if (node.left) queue.push(node.left)
+	}
+	return null;
 }
 
 
 module.exports = {
-    depthFirstSearch
+  depthFirstSearch
 };
